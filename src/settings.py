b444afc0fa057742b5e3557fb4e7f8767f71f372
Berkut <brekutsgaming@mail.ru>
2022-03-08 00:18:30 +0500

    statics

--- src/settings.py
+++ src/settings.py
@@ -122,11 +122,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
+STATIC_ROOT = Path(BASE_DIR / 'staticfiles')
 STATIC_URL = 'static/'
-if not DEBUG:
-    STATIC_ROOT = Path(BASE_DIR / 'static/')
-else:
-    STATICFILES_DIRS = [Path(BASE_DIR / 'static/')]
+STATICFILES_DIRS = [Path(BASE_DIR / 'static/')]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
